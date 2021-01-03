0e6b888c2f56606ed031192a02a886075bd388b9
piyush1008 <piyushsunnyst@gmail.com>
2021-01-03 23:52:33 +0530

    content management added

---
+++ content_management.py
@@ -0,0 +1,6 @@
+def Content():
+    TOPIC_DICT={"BASICS":[["Introduction to Python","/introduction-to-python-programming/"],
+                         ["Print Functions and strings","/python-tutorial-print-function-strings/"],
+                         ["Classes and objects","/classes-and-objects/"]],
+                "Web Dev":[]}
+    return TOPIC_DICT
--- homepage.py
+++ homepage.py
@@ -1,4 +1,8 @@
 from flask import Flask,render_template
+from content_management import Content
+
+TOPIC_DICT=Content()
+
 app=Flask(__name__)
 
 
@@ -6,9 +10,9 @@ app=Flask(__name__)
 def homepage():
     return render_template('home.html')
 
-@app.route('/about')
+@app.route('/about.html')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',TOPIC_DICT=TOPIC_DICT)
 
 
 @app.route('/info.html')
