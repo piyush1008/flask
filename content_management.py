4ba77968b909aaf267732c2b6bb9856011b619f4
piyush1008 <piyushsunnyst@gmail.com>
2021-01-04 11:31:56 +0530

    dashboard added

--- content_management.py
+++ content_management.py
@@ -2,5 +2,8 @@ def Content():
     TOPIC_DICT={"BASICS":[["Introduction to Python","/introduction-to-python-programming/"],
                          ["Print Functions and strings","/python-tutorial-print-function-strings/"],
                          ["Classes and objects","/classes-and-objects/"]],
-                "Web Dev":[]}
+                "Web_Dev":[["HTML Basics","/html-basics"],
+                            ["CSS Basics","/css-basics"],
+                            ["JAVASCRIPT Basics","/script-basics"],
+                            ["JQUERY Basics","/query-basics"]]}
     return TOPIC_DICT
--- homepage.py
+++ homepage.py
@@ -1,9 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,request,url_for,redirect
 from content_management import Content
+import os
 
 TOPIC_DICT=Content()
 
 app=Flask(__name__)
+app.secret_key = os.urandom(24)
 
 
 @app.route('/')
@@ -12,6 +14,7 @@ def homepage():
 
 @app.route('/about.html')
 def about():
+    flash("FLASH TEST")
     return render_template('about.html',TOPIC_DICT=TOPIC_DICT)
 
 
@@ -21,7 +24,28 @@ def info():
 
 @app.route('/register.html')
 def register():
-    return render_template('register.html')
+    return render_template('register.html') 
+
+@app.route('/login.html',methods=['GET','POST'])
+def login():
+    error=None
+    try:
+        if request.method=="POST":
+            attempted_username=request.form['username']
+            attempted_password=request.form['password']
+
+            if attempted_username=="admin" and attempted_password=="pass":
+                return redirect(url_for("about"))
+            else:
+                error="INVALID CREDENTIALS"
+
+        return render_template("login.html",error=error)
+        
+
+    except Exception as e:
+        flash(e)
+        return render_template("login.html",error=error)
+
 
 
 
